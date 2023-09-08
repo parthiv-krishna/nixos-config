ffb669ab918aceea77757f825b3a08f4a7652074
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2023-09-08 00:47:25 -0700

    switch to gnome with dconf

--- environments/desktop.nix
+++ environments/desktop.nix
@@ -8,30 +8,61 @@
   services.xserver = {
     enable = true;
 
+    excludePackages = with pkgs; [
+      xterm
+    ];
+
     # KDE Plasma 5
-    displayManager = {
-      sddm.enable = true;
-      defaultSession = "plasmawayland"; # use Wayland
-    };
-    desktopManager.plasma5.enable = true;
+    # displayManager = {
+    #   sddm.enable = true;
+    #   defaultSession = "plasmawayland"; # use Wayland
+    # };
+    # desktopManager.plasma5.enable = true;
+
+    # GNOME
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
 
     # Configure keymap
     layout = "us";
     xkbVariant = "";
   };
 
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-text-editor
+    gnome-tour
+    kgx
+    orca
+  ]) ++ (with pkgs.gnome; [
+    cheese
+    epiphany
+    file-roller
+    geary
+    gnome-contacts
+    gnome-music
+    gnome-system-monitor
+    totem
+    yelp
+  ]);
+
   home-manager.users.parthiv = {
     imports = [
       ./packages/alacritty.nix
+      ./packages/dconf.nix
     ];
 
     home.packages = with pkgs; [
+      dconf2nix
       discord
+      element-desktop
       firefox
+      gnome.gnome-tweaks
       signal-desktop
       slack
       spotify
       thunderbird
+      vlc
       zoom-us
     ];
   };
---
+++ environments/packages/dconf.nix
@@ -0,0 +1,41 @@
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+{ lib, ... }:
+
+with lib.hm.gvariant;
+
+{
+  dconf.settings = {
+    "org/gnome/desktop/input-sources" = {
+      sources = [ (mkTuple [ "xkb" "us" ]) ];
+      xkb-options = [ "terminate:ctrl_alt_bksp" "caps:escape" ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      clock-show-seconds = false;
+      clock-show-weekday = true;
+      color-scheme = "prefer-dark";
+      font-antialiasing = "grayscale";
+      font-hinting = "slight";
+      text-scaling-factor = 1.25;
+      toolkit-accessibility = true;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-applications = [];
+      switch-applications-backward = [];
+      switch-windows = [ "<Alt>Tab" ];
+      switch-windows-backward = [ "<Shift><Alt>Tab" ];
+    };
+
+    "org/gnome/shell" = {
+      disabled-extensions = [ "window-list@gnome-shell-extensions.gcampax.github.com" "windowsNavigator@gnome-shell-extensions.gcampax.github.com" "workspace-indicator@gnome-shell-extensions.gcampax.github.com" "launch-new-instance@gnome-shell-extensions.gcampax.github.com" "native-window-placement@gnome-shell-extensions.gcampax.github.com" ];
+      enabled-extensions = [ "apps-menu@gnome-shell-extensions.gcampax.github.com" ];
+    };
+
+  };
+}
