8c21ff6fc8c6058ca3434df276c84cbee847d5c1
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2023-11-23 22:12:31 -0800

    amd7040 uses nixos-hardware

--- platforms/framework13-amd7040.nix
+++ platforms/framework13-amd7040.nix
@@ -1,63 +1,24 @@
-# setup specific to Framework Laptop 13 (Intel 13th Gen)
-# based on https://github.com/FrameworkComputer/linux-docs/blob/main/Ubuntu23.04-Manual-Setup-13thGen.md
+# setup specific to Framework Laptop 13 (AMD Ryzen 7040)
 
 { config, lib, pkgs, ... }: {
   imports = [
+    "${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }}/framework/13-inch/7040-amd"
     ./common.nix
   ];
 
   # bluetooth
   hardware.bluetooth.enable = true;
 
-  # enable headset mic input
-  boot.extraModprobeConfig = ''
-    options snd-hda-intel model=dell-headset-multi
-  '';
-
-  # disable ambient light sensor so the brightness keys work
-  hardware.sensor.iio.enable = true;
-  # boot.blacklistedKernelModules = [ "hid-sensor-hub" ];
-  # hardware.acpilight.enable = lib.mkDefault true;
-
-  # set up brightness and airplane mode keys
-  systemd.services.bind-keys-driver = {
-    description = "Set up brightness and airplane mode keys";
-    wantedBy = [ "default.target" ];
-    after = [ "network.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-    };
-    script = ''
-      if [ -e /sys/bus/i2c/devices/i2c-FRMW0001:00 -a ! -e /sys/bus/i2c/drivers/i2c_hid_acpi/i2c-FRMW0001:00 ]; then
-        echo i2c-FRMW0001:00 > /sys/bus/i2c/drivers/i2c_hid_acpi/bind
-        ls -lad /sys/bus/i2c/devices/i2c-*:* /sys/bus/i2c/drivers/i2c_hid_acpi/i2c-*:*
-      fi
-    '';
-  };
-
-  boot.kernelParams = [
-    # power saving: RAM deep sleep
-    "mem_sleep_default=deep"
-    # power saving: disable NVMe ACPI
-    "nvme.noacpi=1"
-    # AMD pstates
-    "amd_pstate=passive"
-  ];
-
   # enable TRIM for SSD
   services.fstrim.enable = lib.mkDefault true;
 
-  # power-profiles-daemon yields better results than TLP for AMD
-  services.power-profiles-daemon.enable = true;
+  # firmware update
+  services.fwupd.enable = true;
 
   # nixos power management
   powerManagement.enable = lib.mkForce true;
 
-  # fingerprint
-  # services.fprintd.enable = lib.mkDefault true;
-
-  # update microcode as needed
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # disable fingerprint
+  services.fprintd.enable = lib.mkForce false;
 }
 
