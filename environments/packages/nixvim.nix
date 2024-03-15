5c1ddb2f98023a37ce49315a2fc697f0e201dd5b
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2024-03-14 19:17:41 -0700

    which-key nvim

--- environments/packages/nixvim.nix
+++ environments/packages/nixvim.nix
@@ -47,24 +47,25 @@ in {
       airline.enable = true;
       barbar.enable = true;
       nvim-tree = {
-      enable = true;
+        enable = true;
 
-      # reclaim Ctrl-K for tmux navigator
-      onAttach = {
-        __raw = ''
-          function(bufnr)
-            local function opts(desc)
-              return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        # reclaim Ctrl-K for tmux navigator
+        onAttach = {
+          __raw = ''
+            function(bufnr)
+              local function opts(desc)
+                return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+              end
+            
+              local api = require("nvim-tree.api")
+              api.config.mappings.default_on_attach(bufnr)
+              vim.keymap.set("n", "<C-K>", ":TmuxNavigateUp<CR>", opts("Refresh"))
             end
-          
-            local api = require("nvim-tree.api")
-            api.config.mappings.default_on_attach(bufnr)
-            vim.keymap.set("n", "<C-K>", ":TmuxNavigateUp<CR>", opts("Refresh"))
-          end
           '';
         };
       };
       todo-comments.enable = true;
+      which-key.enable = true;
 
       # completion
       lspkind.enable = true;
